4e4c8ff6614cc81c25165236ba7bb52983774c49
mohammadKarimi <dot.dotbydot@gmail.com>
2015-05-11 18:53:58 +0430

    Add network in service.ios

---
+++ Trex.Framework.Service/Trex.Framework.Service.iOS/Network/Network.cs
@@ -0,0 +1,41 @@
+namespace Trex.Framework.Service.iOS.Network
+{
+
+    using System;
+    using Trex.Framework.Core.Services.Network;
+    public class Network : INetwork
+    {
+        public NetworkStatus InternetConnectionStatus()
+        {
+            return Reachability.InternetConnectionStatus();
+        }
+        private Action<NetworkStatus> networkStatusChanged;
+        private void HandleReachabilityChanged(object sender, EventArgs e)
+        {
+            var reachabilityChanged = this.networkStatusChanged;
+            if (reachabilityChanged != null)
+            {
+                reachabilityChanged(InternetConnectionStatus());
+            }
+        }
+        public event Action<NetworkStatus> ReachabilityChanged
+        {
+            add
+            {
+                if (networkStatusChanged == null)
+                {
+                    Reachability.ReachabilityChanged += HandleReachabilityChanged;
+                }
+                networkStatusChanged += value;
+            }
+            remove
+            {
+                networkStatusChanged -= value;
+                if (networkStatusChanged == null)
+                {
+                    Reachability.ReachabilityChanged -= HandleReachabilityChanged;
+                }
+            }
+        }
+    }
+}
---
+++ Trex.Framework.Service/Trex.Framework.Service.iOS/Network/Reachability.cs
@@ -0,0 +1,61 @@
+namespace Trex.Framework.Service.iOS.Network
+{
+    using System;
+    using System.Net;
+
+    using CoreFoundation;
+    using SystemConfiguration;
+    using Trex.Framework.Core.Services.Network;
+
+    public static class Reachability
+    {
+        private static NetworkReachability defaultRouteReachability;
+        public static bool IsReachableWithoutRequiringConnection(NetworkReachabilityFlags flags)
+        {
+            return (flags & NetworkReachabilityFlags.Reachable) != 0
+                   && (((flags & NetworkReachabilityFlags.IsWWAN) != 0)
+                       || (flags & NetworkReachabilityFlags.ConnectionRequired) == 0);
+        }
+
+       
+       
+        public static event EventHandler ReachabilityChanged;
+ 
+        public static NetworkStatus InternetConnectionStatus()
+        {
+            NetworkReachabilityFlags flags;
+
+            if ((IsNetworkAvailable(out flags) && ((flags & NetworkReachabilityFlags.IsDirect) != 0)) || flags == 0)
+            {
+                return NetworkStatus.NotReachable;
+            }
+
+            return (flags & NetworkReachabilityFlags.IsWWAN) != 0
+                       ? NetworkStatus.ReachableViaMobileData
+                       : NetworkStatus.ReachableViaWiFi;
+        }
+
+     
+
+        private static void OnChange(NetworkReachabilityFlags flags)
+        {
+            var h = ReachabilityChanged;
+            if (h != null)
+            {
+                h(null, EventArgs.Empty);
+            }
+        }
+
+        public static bool IsNetworkAvailable(out NetworkReachabilityFlags flags)
+        {
+            if (defaultRouteReachability == null)
+            {
+                defaultRouteReachability = new NetworkReachability(new IPAddress(0));
+                defaultRouteReachability.SetNotification(OnChange);
+                defaultRouteReachability.Schedule(CFRunLoop.Current, CFRunLoop.ModeDefault);
+            }
+
+            return defaultRouteReachability.TryGetFlags(out flags) && IsReachableWithoutRequiringConnection(flags);
+        }
+    }
+}
