03bba12cb2071dd21bd4fab9262bf3e5018640d8
mohammadKarimi <dot.dotbydot@gmail.com>
2015-05-11 15:12:08 +0430

    Add TxButtonRenderer And TxEntryRenderer

---
+++ Trex.Framework.Controls/Trex.Framework.Controls.Droid/Extensions/FontExtensions.cs
@@ -0,0 +1,139 @@
+namespace Trex.Framework.Controls.Droid.Controls
+{
+    using System;
+    using System.Collections.Generic;
+    using Android.Content;
+    using Android.Graphics;
+    using Xamarin.Forms.Platform.Android;
+    using Xamarin.Forms;
+
+    public interface ITypefaceCache
+    {
+        void StoreTypeface(string key, Typeface typeface);
+
+        void RemoveTypeface(string key);
+
+        Typeface RetrieveTypeface(string key);
+    }
+    public static class TypefaceCache
+    {
+        private static ITypefaceCache sharedCache;
+        public static ITypefaceCache SharedCache
+        {
+            get
+            {
+                if (sharedCache == null)
+                {
+                    sharedCache = new DefaultTypefaceCache();
+                }
+                return sharedCache;
+            }
+            set
+            {
+                if (sharedCache != null && sharedCache.GetType() == typeof(DefaultTypefaceCache))
+                {
+                    ((DefaultTypefaceCache)sharedCache).PurgeCache();
+                }
+                sharedCache = value;
+            }
+        }
+    }
+    internal class DefaultTypefaceCache : ITypefaceCache
+    {
+        private Dictionary<string, Typeface> _cacheDict;
+        public DefaultTypefaceCache()
+        {
+            _cacheDict = new Dictionary<string, Typeface>();
+        }
+        public Typeface RetrieveTypeface(string key)
+        {
+            if (_cacheDict.ContainsKey(key))
+            {
+                return _cacheDict[key];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void StoreTypeface(string key, Typeface typeface)
+        {
+            _cacheDict[key] = typeface;
+        }
+        public void RemoveTypeface(string key)
+        {
+            _cacheDict.Remove(key);
+        }
+        public void PurgeCache()
+        {
+            _cacheDict = new Dictionary<string, Typeface>();
+        }
+    }
+    public static class FontExtensions
+    {
+
+        /// <summary>
+        /// This method returns typeface for given typeface using following rules:
+        /// 1. Lookup in the cache
+        /// 2. If not found, look in the assets in the fonts folder. Save your font under its FontFamily name. 
+        /// If no extension is written in the family name .ttf is asumed
+        /// 3. If not found look in the files under fonts/ folder
+        /// If no extension is written in the family name .ttf is asumed
+        /// 4. If not found, try to return typeface from Xamarin.Forms ToTypeface() method
+        /// 5. If not successfull, return Typeface.Default
+        /// </summary>
+        /// <returns>The extended typeface.</returns>
+        /// <param name="font">Font</param>
+        /// <param name="context">Android Context</param>
+        public static Typeface ToExtendedTypeface(this Font font, Context context)
+        {
+            Typeface typeface = null;
+            //1. Lookup in the cache
+            var hashKey = font.ToHasmapKey();
+            typeface = TypefaceCache.SharedCache.RetrieveTypeface(hashKey);
+ 
+            //2. If not found, try custom asset folder
+            if (typeface == null && !string.IsNullOrEmpty(font.FontFamily))
+            {
+                string filename = font.FontFamily;
+                //if no extension given then assume and add .ttf
+                if (filename.LastIndexOf(".", System.StringComparison.Ordinal) != filename.Length - 4)
+                {
+                    filename = string.Format("{0}.ttf", filename);
+                }
+                try
+                {
+                    var path = "fonts/" + filename;
+                    typeface = Typeface.CreateFromAsset(context.Assets, path);
+                }
+                catch 
+                {
+                    try
+                    {
+                        typeface = Typeface.CreateFromFile("fonts/" + filename);
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+            //3. If not found, fall back to default Xamarin.Forms implementation to load system font
+            if (typeface == null)
+            {
+                typeface = font.ToTypeface();
+            }
+
+            if (typeface == null)
+            {
+                typeface = Typeface.Default;
+            }
+            TypefaceCache.SharedCache.StoreTypeface(hashKey, typeface);
+            return typeface;
+        }
+
+        private static string ToHasmapKey(this Font font)
+        {
+            return string.Format("{0}.{1}.{2}.{3}", font.FontFamily, font.FontSize, font.NamedSize, (int)font.FontAttributes);
+        }
+    }
+}
