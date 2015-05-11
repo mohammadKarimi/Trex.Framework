89a5c61ae7b16c411087691ce6f7961e6fba912a
mohammadKarimi <dot.dotbydot@gmail.com>
2015-05-11 09:04:59 +0430

    Add TxEntry in Trex.F.Controls Add TextAlignmentExtension class for rtl alignment in Trex.F.Controls

---
+++ Trex.Framework.Controls/Controls/TxEntry/TxEntry.cs
@@ -0,0 +1,58 @@
+﻿namespace Trex.Framework.Controls
+{
+
+    using System;
+    using Xamarin.Forms;
+
+    public class TxEntry : Entry
+    {
+        public static readonly BindableProperty FontProperty = BindableProperty.Create("Font", typeof(Font), typeof(TxEntry), new Font());
+        public Font Font
+        {
+            get { return (Font)GetValue(FontProperty); }
+            set { SetValue(FontProperty, value); }
+        }
+
+        public static readonly BindableProperty XAlignProperty = BindableProperty.Create("XAlign", typeof(TextAlignment), typeof(TxEntry), TextAlignmentExtension.Start);
+        public TextAlignment XAlign
+        {
+            get { return (TextAlignment)GetValue(XAlignProperty); }
+            set { SetValue(XAlignProperty, value); }
+        }
+
+        public static readonly BindableProperty MaxLengthProperty = BindableProperty.Create("MaxLength", typeof(int), typeof(TxEntry), int.MaxValue);
+        public int MaxLength
+        {
+            get { return (int)GetValue(MaxLengthProperty); }
+            set { SetValue(MaxLengthProperty, value); }
+        }
+
+        public static readonly BindableProperty PlaceholderTextColorProperty = BindableProperty.Create("PlaceholderTextColor", typeof(Color), typeof(TxEntry), Color.Default);
+        public Color PlaceholderTextColor
+        {
+            get { return (Color)GetValue(PlaceholderTextColorProperty); }
+            set { SetValue(PlaceholderTextColorProperty, value); }
+        }
+
+        #region __iOS__
+        public EventHandler LeftSwipe;
+        public EventHandler RightSwipe;
+        public void OnLeftSwipe(object sender, EventArgs e)
+        {
+            var handler = this.LeftSwipe;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        public void OnRightSwipe(object sender, EventArgs e)
+        {
+            var handler = this.RightSwipe;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Trex.Framework.Controls/Extensions/TextAlignmentExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Trex.Framework.Controls
+{
+    using Xamarin.Forms;
+    public static class TextAlignmentExtension
+    {
+        public static TextAlignment Start { get { return TextAlignment.End; } }
+        public static TextAlignment Center { get { return TextAlignment.Center; } }
+        public static TextAlignment End { get { return TextAlignment.Start; } }
+    }
+}
