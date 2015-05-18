9aa94e3494a16860058bf03ddfddd72b18bfa452
mohammadKarimi <dot.dotbydot@gmail.com>
2015-05-18 11:05:57 +0430

    remove framework.test winphone and iOS

--- Framework.Test/Framework.Test/App.cs
+++ Framework.Test/Framework.Test/App.cs
@@ -2,42 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Framework.Test
 {
-	public class App : Application
-	{
-		public App ()
-		{
-			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
-		}
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new HttpClientTest();
+        }
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
-
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
-
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+    }
 }
---
+++ Framework.Test/Framework.Test/HttpClientTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Trex.Framework.Core.Serializer;
+using Trex.Framework.Core.Web;
+using Xamarin.Forms;
+
+namespace Framework.Test
+{
+    public class HttpClientTest : ContentPage
+    {
+        public HttpClientTest()
+        {
+            var button = new Button
+            {
+                Text = "Click Me!",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+
+            button.Clicked += async (s, e) =>
+            {
+
+                var HttpRestClient = new HttpRestClient(new Serializer());
+                //var data = new object();
+                //data = new
+                //{
+                //    username = this.UserName,
+                //    password = this.Password
+                //};
+                var ret = await HttpRestClient.GetAsync<HttpActionResult<int>>("http://localhost:59685/oauth/signIn?username=0453711464&password=2908418021");
+                if (ret != null && ret.IsSuccess)
+                {
+                    // Navigation.Push();
+                }
+            };
+
+            Content = button;
+        }
+    }
+}
