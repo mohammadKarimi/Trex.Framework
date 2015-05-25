831b6ae6dfa1f9d652d5372d2c8591c1f6b3eaeb
mohammadKarimi <dot.dotbydot@gmail.com>
2015-05-25 09:41:40 +0430

    Add INavigationService

---
+++ Trex.Framework.Service/Trex.Framework.Service/MVVM/INavigationService.cs
@@ -0,0 +1,10 @@
+﻿namespace Trex.Framework.Service.MVVM
+{
+    using System.Threading.Tasks;
+    using Xamarin.Forms;
+    public interface INavigationService : INavigation
+    {
+        Task<bool> DisplayAlert(string title, string message, string accept = "تائید", string cancel = "انصراف");
+        Task<string> DisplayActionSheet(string title, string cancel, string destruction, params string[] buttons);
+    }
+}
