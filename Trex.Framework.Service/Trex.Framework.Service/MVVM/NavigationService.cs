6f0b8d8f30754a890ba731463751320c2f909ade
mohammadKarimi <dot.dotbydot@gmail.com>
2015-05-25 09:41:53 +0430

    Add NavigationService

---
+++ Trex.Framework.Service/Trex.Framework.Service/MVVM/NavigationService.cs
@@ -0,0 +1,90 @@
+﻿namespace Trex.Framework.Service.MVVM
+{
+    using Xamarin.Forms;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+    public class NavigationService : INavigationService
+    {
+        public Task<string> DisplayActionSheet(string title, string cancel, string destruction, params string[] buttons)
+        {
+            return Page.DisplayActionSheet(title, cancel, destruction, buttons);
+        }
+        public INavigation Navigation { get; set; }
+        public Page Page { get; set; }
+
+        public Task<Page> PopAsync()
+        {
+            return Navigation.PopAsync();
+        }
+
+        public Task<Page> PopModalAsync()
+        {
+            return Navigation.PopModalAsync();
+        }
+
+        public Task PopToRootAsync()
+        {
+            return Navigation.PopToRootAsync();
+        }
+
+        public Task PushAsync(Page page)
+        {
+            return Navigation.PushAsync(page);
+        }
+
+        public Task PushModalAsync(Page page)
+        {
+            return Navigation.PushModalAsync(page);
+        }
+
+        public Task<bool> DisplayAlert(string title, string message, string accept, string cancel = null)
+        {
+            return Page.DisplayAlert(title, message, accept, cancel);
+        }
+
+        public void InsertPageBefore(Page page, Page before)
+        {
+            Navigation.InsertPageBefore(page, before);
+        }
+
+        public IReadOnlyList<Page> ModalStack
+        {
+            get { return Navigation.ModalStack; }
+        }
+
+        public IReadOnlyList<Page> NavigationStack
+        {
+            get { return Navigation.NavigationStack; }
+        }
+
+        public Task<Page> PopAsync(bool animated)
+        {
+            return Navigation.PopAsync(animated);
+        }
+
+        public Task<Page> PopModalAsync(bool animated)
+        {
+            return Navigation.PopModalAsync(animated);
+        }
+
+        public Task PopToRootAsync(bool animated)
+        {
+            return Navigation.PopToRootAsync(animated);
+        }
+
+        public Task PushAsync(Page page, bool animated)
+        {
+            return Navigation.PushAsync(page, animated);
+        }
+
+        public Task PushModalAsync(Page page, bool animated)
+        {
+            return Navigation.PushModalAsync(page, animated);
+        }
+
+        public void RemovePage(Page page)
+        {
+            Navigation.RemovePage(page);
+        }
+    }
+}
