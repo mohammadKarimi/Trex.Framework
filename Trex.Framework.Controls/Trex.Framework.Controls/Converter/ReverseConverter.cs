c4c4f159c65c161b788ed669144d3ec56bc5513d
mohammadKarimi <dot.dotbydot@gmail.com>
2015-05-25 09:41:17 +0430

    Add reverse converter

---
+++ Trex.Framework.Controls/Trex.Framework.Controls/Converter/ReverseConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace Trex.Framework.Controls.Converter
+{
+    using System;
+    using System.Globalization;
+    using Xamarin.Forms;
+    public class ReverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                return !(bool)value;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
