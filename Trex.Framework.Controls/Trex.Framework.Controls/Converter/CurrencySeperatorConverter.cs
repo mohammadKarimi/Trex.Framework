80908be8a0ac5d64dab17d84eb483d4e70346373
mohammadKarimi <dot.dotbydot@gmail.com>
2015-05-31 11:40:15 +0430

    add currency seperator

---
+++ Trex.Framework.Controls/Trex.Framework.Controls/Converter/CurrencySeperatorConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace Trex.Framework.Controls.Converter
+{
+    using System;
+    using Xamarin.Forms;
+    public class CurrencySeperatorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is decimal)
+            {
+                var currency = ((decimal)value).ToString("0,0");
+                return currency.ToString();
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
