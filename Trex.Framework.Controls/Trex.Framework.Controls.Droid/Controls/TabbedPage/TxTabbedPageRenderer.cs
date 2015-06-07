6b8c592af2992f525887d9297fdfd2e227a9118d
mohammadKarimi <dot.dotbydot@gmail.com>
2015-06-07 18:41:28 +0430

    add tabbedpagerenderer for android

---
+++ Trex.Framework.Controls/Trex.Framework.Controls.Droid/Controls/TabbedPage/TxTabbedPageRenderer.cs
@@ -0,0 +1,17 @@
+﻿namespace Trex.Framework.Controls.Droid
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+    public class TxTabbedPageRenderer : TabbedRenderer
+    {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<TabbedPage> e)
+        {
+            base.OnElementChanged(e);
+        }
+    }
+}
