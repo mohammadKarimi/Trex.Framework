bf108d9a54096ae0bbe438a9791077b8e4a1e9d0
mohammadKarimi <dot.dotbydot@gmail.com>
2015-06-20 16:38:09 +0430

    debug serializer

--- Trex.Framework.Core/Serializer/Serializer.cs
+++ Trex.Framework.Core/Serializer/Serializer.cs
@@ -8,7 +8,7 @@
     {
         public string Serialize<T>(T obj)
         {
-            return JsonConvert.SerializeObject(obj);
+                return JsonConvert.SerializeObject(obj);
         }
 
         public T Deserialize<T>(string data)
